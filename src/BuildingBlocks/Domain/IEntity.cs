70382137ad0f732ed9608cb04719a1ebd9042d80
Thai Mai <63966546+relentless-spirit@users.noreply.github.com>
2026-03-06 23:27:19 +0700

    Remove comments from IEntity interface
    Removed comments from IEntity interface.

--- src/BuildingBlocks/Domain/IEntity.cs
+++ src/BuildingBlocks/Domain/IEntity.cs
@@ -2,9 +2,7 @@ namespace BuildingBlocks.Domain;
 
 public interface IEntity
 {
-    // 1. Để lấy danh sách Domain Events mà không cần quan tâm ID là int hay Guid
     IReadOnlyCollection<IDomainEvent> DomainEvents { get; }
     
-    // 2. Để clear events sau khi save xong
     void ClearDomainEvents();
 }
